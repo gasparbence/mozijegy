96f5722def734460ec665ea103babbfc26e3630e
Barnus3732 <barnabaskarker@gmail.com>
2024-02-29 09:12:28 +0100

    foablak elkezdve

---
+++ fooldal.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import messagebox
+from ttkbootstrap import *
+
+ablak=Tk()
+ablak.resizable(False, False)
+ablak.title("Mozifalva")
+ablak.geometry("700x650")
+
+
+
+
+
+
+ablak.mainloop()
