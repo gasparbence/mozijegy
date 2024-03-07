b9475497860c76c9c451ba2df27fab7c4770de6f
Karker Barnabás <barnabaskarker@gmail.com>
2024-03-07 09:08:35 +0100

    haladás

--- fooldal.py
+++ fooldal.py
@@ -7,8 +7,10 @@ ablak=Tk()
 ablak.resizable(False, False)
 ablak.title("Mozifalva")
 ablak.geometry("800x800")
-
-
+ablak.configure(bg="#2a1a1d")
+logo = PhotoImage(file="Mozifalva.png")
+kep = Label(ablak, image=logo)
+kep.place(x=400, y=200, anchor=CENTER)
 fogomb = Button(ablak, bootstyle="dark", text="Belépés", width=30)
 fogomb.place(x=400, y=700, anchor=CENTER)
 
