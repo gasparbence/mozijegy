4cda2fc9e03bb83558aeec0f36e618c10af1b698
Karker Barnabás <barnabaskarker@gmail.com>
2024-03-14 09:22:39 +0100

    maiszar

--- fooldal.py
+++ fooldal.py
@@ -2,16 +2,20 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
 from ttkbootstrap import *
+from PIL import Image
 
-ablak=Window()
+
+ablak=Window(themename='lumen')
 ablak.resizable(False, False)
 ablak.title("Mozifalva")
 ablak.geometry("800x800")
 ablak.configure(background="#2a1a1d")
 logo = PhotoImage(file="Mozifalva.png")
 kep = Label(ablak, image=logo)
-kep.place(x=400, y=200, anchor=CENTER)
-fogomb = Button(ablak, bootstyle="dark", text="Belépés", width=30)
+kep.place(x=400, y=300, anchor=CENTER)
+mystyle=Style()
+mystyle.configure("dark.Outline.TButton")
+fogomb = Button(ablak, bootstyle="dark", style="dark.Outline.TButton", text="BELÉPÉS", width=30)
 fogomb.place(x=400, y=700, anchor=CENTER)
 
 
