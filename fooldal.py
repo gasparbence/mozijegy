05f0617532434afebaf5b579e4a0c38a9268e06d
Karker Barnabás <barnabaskarker@gmail.com>
2024-03-21 09:05:17 +0100

    logokesz

--- fooldal.py
+++ fooldal.py
@@ -2,20 +2,20 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
 from ttkbootstrap import *
-from PIL import Image
 
 
-ablak=Window(themename='lumen')
+
+ablak=Window(themename="vapor")
 ablak.resizable(False, False)
 ablak.title("Mozifalva")
 ablak.geometry("800x800")
-ablak.configure(background="#2a1a1d")
-logo = PhotoImage(file="Mozifalva.png")
-kep = Label(ablak, image=logo)
+ablak.configure(background="black")
+logo = PhotoImage(file="popcorntownfinal.png")
+kep = Label(ablak, image=logo , borderwidth=0)
 kep.place(x=400, y=300, anchor=CENTER)
 mystyle=Style()
-mystyle.configure("dark.Outline.TButton")
-fogomb = Button(ablak, bootstyle="dark", style="dark.Outline.TButton", text="BELÉPÉS", width=30)
+mystyle.configure("danger.TButton")
+fogomb = Button(ablak, bootstyle="danger", style="danger.TButton", text="BELÉPÉS", width=30)
 fogomb.place(x=400, y=700, anchor=CENTER)
 
 
