a43071f26ddfa69d8ecc3a1059aeb3436f1f3571
Gáspár Bence <gasparbence0915@gmail.com>
2024-04-11 07:23:50 +0200

    asd

--- jegyrendelés.py
+++ jegyrendelés.py
@@ -3,19 +3,29 @@ from tkinter.ttk import *
 from tkinter import messagebox
 from ttkbootstrap import *
 
-ablak=Tk()
+
+ablak=Window(themename="vapor")
 ablak.resizable(False, False)
 ablak.title("Mozifalva")
 ablak.geometry("800x800")
-ablak.configure(bg="#2a1a1d")
-logo = PhotoImage(file="kepek/Mozifalva.png")
-kep = Label(ablak, image=logo)
-kep.place(x=400, y=200, anchor=CENTER)
-fogomb = Button(ablak, bootstyle="dark", text="Belépés", width=30)
+ablak.configure(background="black")
+canv2 = Canvas(ablak, width=400, height=400, bg='white')
+canv2.grid(row=1, column=0)
+kep3 = Image.open("./kepek/popcorntownfinal.png")
+img2 = ImageTk.PhotoImage(kep3) 
+canv2.create_image(20, 20, anchor=NW, image=img2)
+mystyle=Style()
+mystyle.configure("danger.TButton")
+
+def belepes():
+    topk=Toplevel()
+    topk.geometry("400x400")
+    topk.title("Filmek")
+    filmektabla=Label(topk,text="Választható filmek:")
+    filmektabla.grid(row=0, column=0)
+
+fogomb = Button(ablak, bootstyle="danger", style="danger.TButton", text="BELÉPÉS", width=30, command=belepes)
 fogomb.place(x=400, y=700, anchor=CENTER)
-
-
-
 
 
 
