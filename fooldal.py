daa75364e297873b693488f6ecd08872c9bf19be
Karker Barnabás <barnabaskarker@gmail.com>
2024-04-11 07:27:15 +0200

    egyben

--- fooldal.py
+++ fooldal.py
@@ -4,6 +4,13 @@ from tkinter import messagebox
 from ttkbootstrap import *
 
 
+def belepes():
+    topk=Toplevel()
+    topk.geometry("800x800")
+    topk.title("Filmek")
+    topk.configure(background="black")
+    filmektabla=Label(topk, text="Választható filmek:")
+    filmektabla.grid(row=0, column=0)
 
 
 ablak=Window(themename="vapor")
@@ -16,11 +23,8 @@ kep = Label(ablak, image=logo , borderwidth=0)
 kep.place(x=400, y=300, anchor=CENTER)
 mystyle=Style()
 mystyle.configure("danger.TButton")
-fogomb = Button(ablak, bootstyle="danger", style="danger.TButton", text="BELÉPÉS", width=30)
+fogomb = Button(ablak, bootstyle="danger", style="danger.TButton", text="BELÉPÉS", width=30, command=belepes)
 fogomb.place(x=400, y=700, anchor=CENTER)
-
-
-
 
 
 
